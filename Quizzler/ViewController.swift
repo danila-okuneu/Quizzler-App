1bf611f595a82d4d420deef42aa3f90cbcf37d59
Danila Okuneu <okuneu@icloud.com>
2023-08-02 10:29:41 +0300

    add project files with interface

---
+++ Quizzler/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Quizzler
+//
+//  Created by Danila Okuneu on 2.08.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func buttonPressed(_ sender: UIButton) {
+    }
+    
+}
+
